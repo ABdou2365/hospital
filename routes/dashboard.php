48e39939a15d9e340f48ea4ec1330b68c2d70da9
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2023-07-18 20:03:04 +0100

    user auth

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('Dashboard_Admin', [DashboardController::class,'index']);
 
-Route::get('/dashboard_', function () {
+Route::get('/dashboard/user', function () {
     return view('Dashboard.User.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard.user');
 
 
 require __DIR__.'/auth.php';
